326c14791b752d8eabf817efd4789575e53d6214
Nick <skcin7@diogenes.local>
2021-01-15 21:37:06 -0800

    Added back contact card download.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,6 +6,6 @@
     <div class="container">
         <p>The best way to contact me for now is via email (or DM me on Instagram). Usually but not always I will respond within 24 hours (if not way sooner).</p>
         <p><strong>Email:</strong> <a href="mailto:nick@nicholas-morgan.com">nick@nicholas-morgan.com</a></p>
-        <p>You can also <a href="{{ route('contact_card') }}">download my Contact Card</a> (use this to add to your mobile device Contacts list).</p>
+        <p>You can also <a href="{{ route('contact_card') }}">download my Contact Card</a> (add this to your mobile device Contacts list).</p>
     </div>
 @endsection

c35f81c8116c62f4e2366c5f8f663e1d7728c673
Nick <skcin7@diogenes.local>
2021-01-15 16:11:04 -0800

    Added basic message to welcome page.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container">
-        <p>The best way to contact me for now is via email or slide into my DMs on Instagram.<br/><br/>
+        <p>The best way to contact me for now is via email (or slide into my DMs on Instagram). Usually but not always I respond within 24 hours if not way sooner.<br/><br/>
             <a href="mailto:nick@nicholas-morgan.com">nick@nicholas-morgan.com</a></p>
     </div>
 @endsection

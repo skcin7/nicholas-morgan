eac196bb1cd339ac0ab3cc7a8854473072e7afae
Nick <skcin7@diogenes.local>
2021-01-15 18:32:04 -0800

    Minor contact info change.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="container">
-        <p>The best way to contact me for now is via email (or slide into my DMs on Instagram). Usually but not always I will respond within 24 hours (if not way sooner).</p>
+        <p>The best way to contact me for now is via email (or DM me on Instagram). Usually but not always I will respond within 24 hours (if not way sooner).</p>
         <p><strong>Email:</strong> <a href="mailto:nick@nicholas-morgan.com">nick@nicholas-morgan.com</a></p>
 {{--        <p>You can also <a href="{{ route('contact_card') }}">download my Contact Card</a>.</p>--}}
     </div>

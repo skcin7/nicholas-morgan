bfd019f1c6936a30a6cf6bee35cc639d27807fcc
Nick <skcin7@diogenes.local>
2021-01-15 17:49:15 -0800

    Removed Contact Card Link

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -6,6 +6,6 @@
     <div class="container">
         <p>The best way to contact me for now is via email (or slide into my DMs on Instagram). Usually but not always I will respond within 24 hours (if not way sooner).</p>
         <p><strong>Email:</strong> <a href="mailto:nick@nicholas-morgan.com">nick@nicholas-morgan.com</a></p>
-        <p>You can also <a href="{{ route('contact_card') }}">download my Contact Card</a>.</p>
+{{--        <p>You can also <a href="{{ route('contact_card') }}">download my Contact Card</a>.</p>--}}
     </div>
 @endsection

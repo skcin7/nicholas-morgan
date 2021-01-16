ab79cb2b5cf2b465a8b7b9fbef4331bf7e480c2f
Nick <skcin7@diogenes.local>
2021-01-15 16:51:27 -0800

    Updated about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
 
         <h1>About Nick</h1>
 
-        <p><strong>I am a Software Engineer, Business Owner, and Video Game Collector/Preservationist</strong> currently living in California, USA (and originally from New Jersey, USA). I'll write more cool stuff here later to help better say a basic introduction/summary. //TODO write more cool stuff here later</p>
+        <p><strong>I am a Software Engineer, Business Owner, and Video Game Collector/Preservationist</strong> currently living in California, USA (and originally from New Jersey, USA). I'll write more cool stuff here later to help better say a basic introduction/summary of who I am. //TODO write more cool stuff here later</p>
 
     </div>
 @endsection

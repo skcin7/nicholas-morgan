687cf3041e8792d7a354db5298f6eb7a9308cb76
Nick <skcin7@diogenes3.local>
2022-01-19 14:31:10 -0800

    Default git commit message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,6 @@
 @section('content')
     <div class="container-fluid">
         <h2 class="text-center">Welcome</h2>
-{{--        <p>I am a <strong>computer programmer</strong>, <strong>retro video game collector</strong>, and <strong>business owner</strong> that currently lives in California, USA, and I'm originally from New Jersey, USA. Thanks for visiting!</p>--}}
+        <p>I am a <strong>computer programmer</strong>, <strong>retro video game collector</strong>, and <strong>business owner</strong> that currently lives in California, USA, and I'm originally from New Jersey, USA. Thanks for visiting!</p>
     </div>
 @endsection
